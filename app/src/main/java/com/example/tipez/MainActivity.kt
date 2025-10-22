37e936e9e161b0c8a365618e336e82567f322c25
Amadeus Kaczmarek <amadeuskaczmarek@gmail.com>
2025-10-22 01:03:25 -0600

    Variables corresponding to id's

--- app/src/main/java/com/example/tipez/MainActivity.kt
+++ app/src/main/java/com/example/tipez/MainActivity.kt
@@ -1,20 +1,30 @@
 package com.example.tipez
 
 import android.os.Bundle
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var etBaseAmount: EditText
+    private lateinit var seekbarTip: EditText
+    private lateinit var tvTipPercent: EditText
+    private lateinit var tvTipAmount: EditText
+    private lateinit var tvTotalAmount: EditText
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        etBaseAmount = findViewById(R.id.etBaseAmount)
+        seekbarTip = findViewById(R.id.seekBarTip)
+        tvTipPercent = findViewById(R.id.tvTipPercentLabel)
+        tvTipAmount = findViewById(R.id.tvTipAmount)
+        tvTotalAmount = findViewById(R.id.tvTotalAmount)
+
+
     }
 }
