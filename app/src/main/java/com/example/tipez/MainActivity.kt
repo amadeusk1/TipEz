accef34ad2b760f8c31f986d3b50b132dbac6533
Amadeus Kaczmarek <amadeuskaczmarek@gmail.com>
2025-10-22 01:20:42 -0600

    Seekbar show value in console

--- app/src/main/java/com/example/tipez/MainActivity.kt
+++ app/src/main/java/com/example/tipez/MainActivity.kt
@@ -1,18 +1,23 @@
 package com.example.tipez
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.EditText
+import android.widget.SeekBar
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
+private const val TAG = "MainActivity"
+
 class MainActivity : AppCompatActivity() {
     private lateinit var etBaseAmount: EditText
-    private lateinit var seekbarTip: EditText
-    private lateinit var tvTipPercent: EditText
-    private lateinit var tvTipAmount: EditText
-    private lateinit var tvTotalAmount: EditText
+    private lateinit var seeBarTip: SeekBar
+    private lateinit var tvTipPercentLabel: TextView
+    private lateinit var tvTipAmount: TextView
+    private lateinit var tvTotalAmount: TextView
 
 
 
@@ -20,11 +25,24 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         etBaseAmount = findViewById(R.id.etBaseAmount)
-        seekbarTip = findViewById(R.id.seekBarTip)
-        tvTipPercent = findViewById(R.id.tvTipPercentLabel)
+        seeBarTip = findViewById(R.id.seekBarTip)
+        tvTipPercentLabel = findViewById(R.id.tvTipPercentLabel)
         tvTipAmount = findViewById(R.id.tvTipAmount)
         tvTotalAmount = findViewById(R.id.tvTotalAmount)
 
+        seeBarTip.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener {
 
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                Log.i(TAG, "onPrpgressChanged $progress")
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+            }
+
+
+        })
     }
 }
